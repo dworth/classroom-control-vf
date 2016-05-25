ec91abfad08a6393391623629ec3b7423d87943f
dworth <david.worth@cslbehring.com>
2016-05-25 13:17:20 -0400

    ee

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -3,13 +3,5 @@ class users {
   user {'fundamentals':
     ensure => present,
   }
-   user {'jose':
-    ensure => present,
-  }
-   user {'chen':
-    ensure => present,
-  }
-   user {'alice':
-    ensure => present,
-  }
+   
 }
