27bd52706f7c80246f5d7524a52c215319e93e76
dworth <david.worth@cslbehring.com>
2016-05-26 09:48:54 -0400

    111

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,8 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
+  ##notify { "Hello, my name is ${::hostname}": }
+  notify {$message:}
   
   if $::virtual != 'physical' {
     $vmname = capitalize($::virtual)
